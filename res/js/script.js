ea5f467db0d20e1e35afc90c28dd8c24851cbb20
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-01-06 19:38:16 +0530

    Add speaker event links and update speaker images

--- res/js/script.js
+++ res/js/script.js
@@ -11,13 +11,24 @@ Vue.createApp({
             {
                 name: " Dananjaya Hettiarachchi",
                 image: "./res/images/speakers/dhananjaya_hettiarachchi.webp",
-                description: "Dananjaya Hettiarachchi World Champion of Public Speaking 2014"
+                description: "World Champion of Public Speaking 2014",
+                event: "Dream Big 2021",
+                link:"http://gavel.mrt.ac.lk/dream-big-2021-the-art-of-storytelling/"
             },
             {
                 name:"Peter D'Almeida",
-                image:"./res/images/speakers/peter_dalmeida.png",
-                description:"Peter D'Almeida Founder and CEO of N-Able"
+                image:"./res/images/speakers/peter.webp",
+                description:"Founder and CEO of N-Able",
+                event: "Speech Olympiad 2021",
+                link:"http://gavel.mrt.ac.lk/finals-speech-olympiad-xiv/"
             },
+            {
+              name:"Santhush Weeraman",
+              image:"./res/images/speakers/santhush.webp",
+              description:"Singer, Composer, Entrepreneur",
+              event: "Speech Olympiad 2023",
+              link:"https://www.facebook.com/media/set?vanity=speech.olympiad.uom&set=a.758347263006290"
+            }
         ]
     };
   },
